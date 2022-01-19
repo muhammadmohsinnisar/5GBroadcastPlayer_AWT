f7e178cfbeea8b65ad4c185b3eadffb79bdab053
mohsinnisar <mohxinbutt00@gmail.com>
2022-01-19 23:04:11 +0100

    * Added Channel List with clickable cardview and source * Added Glide for fetching image on channel list * Still need to look into stopping the stream on backpressed * Added fullscreen option but does not work yet. * Worked on finishing stream on back pressed

--- app/src/main/java/com/example/a5gbroadcastplayer_awt/PlayerActivity.java
+++ app/src/main/java/com/example/a5gbroadcastplayer_awt/PlayerActivity.java
@@ -1,10 +1,15 @@
 package com.example.a5gbroadcastplayer_awt;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.cursoradapter.widget.SimpleCursorAdapter;
 
 import com.google.android.exoplayer2.C;
@@ -15,6 +20,7 @@ import com.google.android.exoplayer2.source.DefaultMediaSourceFactory;
 import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.source.MediaSourceFactory;
 import com.google.android.exoplayer2.source.ProgressiveMediaSource;
+import com.google.android.exoplayer2.ui.AspectRatioFrameLayout;
 import com.google.android.exoplayer2.ui.PlayerView;
 import com.google.android.exoplayer2.upstream.DefaultDataSource;
 
@@ -23,6 +29,8 @@ public class PlayerActivity extends AppCompatActivity{
     private static final String STREAM_URL = "http://ftp.itec.aau.at/datasets/DASHDataset2014/TearsOfSteel/2sec/TearsOfSteel_2s_onDemand_2014_05_09.mpd";
     ExoPlayer player;
     PlayerView playerView;
+    ImageView fullScreenButton;
+    boolean fullScreen = false;
     // creating a variable for exoplayer
     // Need to add a binder to the view
     //The context might be cause of failure
@@ -35,21 +43,76 @@ public class PlayerActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
         playerView =  new PlayerView(getApplicationContext());
-        playerView.findViewById(R.id.exo_player_view);
+        playerView.findViewById(R.id.player);
+        fullScreenButton = playerView.findViewById(R.id.exo_fullscreen_icon);
+
+
         playerView.setPlayer(player);
+        playerView.setResizeMode(AspectRatioFrameLayout.RESIZE_MODE_FIXED_HEIGHT);
         Bundle extras = getIntent().getExtras();
         String source = extras.getString("source");
-        ChannelActivity channelActivity = new ChannelActivity();
+        /*fullScreenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(fullScreen){
+                    fullScreenButton.setImageDrawable(ContextCompat.getDrawable(PlayerActivity.this,R.drawable.ic_fullscreen_open));
+                    getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
+
+                    if(getSupportActionBar() !=null){
+                        getSupportActionBar().show();
+                    }
+
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+                    RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) playerView.getLayoutParams();
+                    params.width = params.MATCH_PARENT;
+                    params.height = (int) ( 200 * getApplicationContext().getResources().getDisplayMetrics().density);
+                    playerView.setLayoutParams(params);
+
+                    fullScreen = false;
+
+                }else {
+                    fullScreenButton.setImageDrawable(ContextCompat.getDrawable(PlayerActivity.this,R.drawable.ic_fullscreen_close));
+
+                    getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN
+                            |View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                            |View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
+                    if(getSupportActionBar() != null){
+                        getSupportActionBar().hide();
+                    }
+
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+                    RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) playerView.getLayoutParams();
+                    params.width = params.MATCH_PARENT;
+                    params.height = params.MATCH_PARENT;
+                    playerView.setLayoutParams(params);
+
+                    fullScreen = true;
+                }
+            }
+        });*/
         initializePlayer(source);
+
     }
+
+
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
         Intent intent2 = new Intent(getApplicationContext(),ChannelActivity.class);
         stopPlayer();
-        PlayerActivity.this.finish();
+        player.release();
+        onDestroy();
         startActivity(intent2);
+    }
+
+    @Override
+    public void onDestroy(){
+        player.release();
+        super.onDestroy();
     }
 
     private void initializePlayer(String source){
@@ -62,12 +125,14 @@ public class PlayerActivity extends AppCompatActivity{
                 .setMediaSourceFactory(mediaSourceFactory)
                 .build();
 
+
         ExoPlayer simpleExoPlayer = new ExoPlayer.Builder(this).build();
-        PlayerView playerView = findViewById(R.id.exo_player_view);
+        PlayerView playerView = findViewById(R.id.player);
         playerView.setPlayer(simpleExoPlayer);
         MediaItem mediaItem = MediaItem.fromUri(source);
         simpleExoPlayer.addMediaItem(mediaItem);
         simpleExoPlayer.prepare();
+        simpleExoPlayer.setPlayWhenReady(true);
     }
 
     private void stopPlayer(){
