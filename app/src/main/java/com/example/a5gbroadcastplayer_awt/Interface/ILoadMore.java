5bcc55ca6f96e5ded347176d6a8cd3954632633c
mohsinnisar <mohxinbutt00@gmail.com>
2022-01-24 16:57:27 +0100

    Merge remote-tracking branch 'origin/master'
    * Add material.io to UI

---
+++ app/src/main/java/com/example/a5gbroadcastplayer_awt/Interface/ILoadMore.java
@@ -0,0 +1,5 @@
+package com.example.a5gbroadcastplayer_awt.Interface;
+
+public interface ILoadMore {
+    void onLoadMore();
+}
---
+++ app/src/main/java/com/example/a5gbroadcastplayer_awt/Model/Item.java
@@ -0,0 +1,28 @@
+package com.example.a5gbroadcastplayer_awt.Model;
+
+public class Item {
+    private String name;
+    private int length;
+
+    public Item(String name, int length){
+        this.name = name;
+        this.length = length;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(){
+    }
+
+    public int getLength(){
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+
+}
