e677f98929dff6fdd26174e9af48292b35fbcf0b
mohsinnisar <mohsin.nisar@fokus.fraunhofer.de>
2022-02-10 15:49:36 +0100

    Try changing the model

--- app/src/main/java/Model/CustomModel.java
+++ app/src/main/java/Model/CustomModel.java
@@ -1,2 +1,26 @@
-package Model;public class CustomModel {
+package Model;
+
+public class CustomModel {
+    private String channelName = null;
+    private String channelUrl = null;
+
+    public CustomModel(){
+
+    }
+
+    public String getChannelName(){
+        return channelName;
+    }
+
+    public void setChannelName(String channelName) {
+        this.channelName = channelName;
+    }
+
+    public String getChannelUrl(){
+        return channelUrl;
+    }
+
+    public void setChannelUrl(String channelUrl){
+        this.channelUrl = channelUrl;
+    }
 }
