b4d3d0e12365e3a6fc60aedc01e1aba0f51d8f66
mohsinnisar <mohsin.nisar@fokus.fraunhofer.de>
2022-02-13 19:17:31 +0100

    Fullscreen solved

--- app/src/main/java/com/example/a5gbroadcastplayer_awt/PlayerActivity.java
+++ app/src/main/java/com/example/a5gbroadcastplayer_awt/PlayerActivity.java
@@ -2,32 +2,25 @@ package com.example.a5gbroadcastplayer_awt;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.SurfaceControl;
 import android.view.View;
-import android.widget.FrameLayout;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
-import androidx.cursoradapter.widget.SimpleCursorAdapter;
 
-import com.google.android.exoplayer2.C;
 import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.MediaItem;
-import com.google.android.exoplayer2.Player;
-import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.source.DefaultMediaSourceFactory;
-import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.source.MediaSourceFactory;
-import com.google.android.exoplayer2.source.ProgressiveMediaSource;
-import com.google.android.exoplayer2.ui.AspectRatioFrameLayout;
 import com.google.android.exoplayer2.ui.PlayerControlView;
 import com.google.android.exoplayer2.ui.PlayerView;
-import com.google.android.exoplayer2.ui.StyledPlayerControlView;
 import com.google.android.exoplayer2.upstream.DefaultDataSource;
 
 public class PlayerActivity extends AppCompatActivity implements View.OnClickListener{
@@ -38,8 +31,6 @@ public class PlayerActivity extends AppCompatActivity implements View.OnClickLis
     //ExoPlayer player;
     PlayerView playerView;
     PlayerControlView playerControlView;
-    ImageView fullscreenButton;
-    boolean fullscreen = false;
     String channelName, channelUrl;
     // creating a variable for exoplayer
     // Need to add a binder to the view
@@ -55,59 +46,52 @@ public class PlayerActivity extends AppCompatActivity implements View.OnClickLis
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature( Window.FEATURE_NO_TITLE );
+
+        getWindow().setFlags( WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN );
         setContentView(R.layout.activity_frame_player);
         channelName = getIntent().getExtras().getString("name");
         channelUrl = getIntent().getExtras().getString("url");
 
         playerView =  new PlayerView(getApplicationContext());
-        playerControlView = new PlayerControlView(getApplicationContext());
+        //playerControlView = new PlayerControlView(getApplicationContext());
         playerView.findViewById(R.id.player);
-        playerControlView.findViewById(R.id.controls);
-        playerControlView.setPlayer(simpleExoPlayer);
+        //playerControlView.findViewById(R.id.controls);
+        //playerControlView.setPlayer(simpleExoPlayer);
         playerView.setPlayer(simpleExoPlayer);
 
 
         //fullscreenButton = playerView.findViewById(R.id.exo_fullscreen_icon);
 
-        /*fullscreenButton.setOnClickListener(new View.OnClickListener() {
+      /* fullscreenButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),"The fullscreen click!!",Toast.LENGTH_LONG).show();
                 if(fullscreen) {
                     fullscreenButton.setImageDrawable(ContextCompat.getDrawable(PlayerActivity.this, R.drawable.ic_fullscreen_open));
-
                     getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
-
                     if(getSupportActionBar() != null){
                         getSupportActionBar().show();
                     }
-
-                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
                     RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) playerView.getLayoutParams();
                     params.width = params.MATCH_PARENT;
                     params.height = (int) ( 200 * getApplicationContext().getResources().getDisplayMetrics().density);
                     playerView.setLayoutParams(params);
-
                     fullscreen = false;
                 }else{
                     fullscreenButton.setImageDrawable(ContextCompat.getDrawable(PlayerActivity.this, R.drawable.ic_fullscreen_close));
-
                     getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN
                             |View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                             |View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-
                     if(getSupportActionBar() != null){
                         getSupportActionBar().hide();
                     }
-
                     setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-
-                    FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) playerView.getLayoutParams();
+                    RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) playerView.getLayoutParams();
                     params.width = params.MATCH_PARENT;
                     params.height = params.MATCH_PARENT;
                     playerView.setLayoutParams(params);
-
                     fullscreen = true;
                 }
             }
